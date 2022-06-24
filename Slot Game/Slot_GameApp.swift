4c5da6f984535ca5c9ac3ccbaad93f66db7c6c82
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2022-06-24 11:21:33 -0300

    Initial Commit

---
+++ Slot Game/Slot_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_GameApp.swift
+//  Slot Game
+//
+//  Created by Manny Alvarez on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Slot_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
